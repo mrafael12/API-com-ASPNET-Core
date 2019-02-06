628623327cc56a7483616273d37fc4fd0df2b243
Marcos Rafael de Sousa Paula <rafael.paula@textiluniao.com.br>
2019-02-06 15:26:40 -0300

    Criando Person Teste

---
+++ APIWithASPNETCore Using Diferent Verbs/APIWithASPNETCore/Services/Implementattions/PersonServiceImpl.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIWithASPNETCore.Model;
+using System.Threading;
+
+namespace APIWithASPNETCore.Services
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementGet(),
+                FirstName = "Rafael",
+                LastName = "Paula",
+                Address = "Fortaleza - Ceará - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
